214fc6b8468f808270e5d97df35526c71187db82
upokusaev <upokusaev@gmail.com>
2019-09-09 12:39:01 +0400

    Step 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,4 +5,5 @@ var program = require('commander');
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
