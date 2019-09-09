ba8779dd965d0dc85ba19c26d794b755a573cdb2
upokusaev <upokusaev@gmail.com>
2019-09-09 11:26:45 +0400

    Step 1

---
+++ Makefile
@@ -0,0 +1,10 @@
+install:
+	npm install
+start:
+	npx babel-node src/bin/gendiff.js
+publish:
+	npm publish --dry-run
+test:
+	npm test
+lint:
+	npx eslint .
---
+++ src/bin/gendiff.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .parse(process.argv);
