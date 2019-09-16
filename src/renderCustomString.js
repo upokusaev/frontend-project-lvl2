a4d8c1464db0f1d2b44931786d5d8947b962e03b
upokusaev <upokusaev@gmail.com>
2019-09-16 23:24:18 +0400

    refactoring test coverage

--- src/renderCustomString.js
+++ src/renderCustomString.js
@@ -4,9 +4,8 @@ const getPrefix = (obj) => {
   switch (obj.mod) {
     case 'removed': return '  - ';
     case 'added': return '  + ';
-    case 'not': return '    ';
     case 'updated': return ['  - ', '  + '];
-    default: return '';
+    default: return '    ';
   }
 };
 
@@ -16,7 +15,11 @@ const renderObj = (obj, level) => {
   return `{\n${res.join('\n')}\n${tab.repeat(level)}}`;
 };
 
-const getValue = (value, level) => {
+const getValue = (value, level, prefix) => {
+  if (value instanceof Array) {
+    const [value1, value2] = [getValue(value[0], level), getValue(value[1], level)];
+    return `${tab.repeat(level)}${prefix[0]}${obj.name}: ${value1}\n${tab.repeat(level)}${prefix[1]}${obj.name}: ${value2}`;
+  }
   if (value instanceof Object) return renderObj(value, level + 1);
   return value;
 };
@@ -25,14 +28,12 @@ const render = (diff, level = 0) => {
   const res = diff.map((obj) => {
     const prefix = getPrefix(obj);
     const startStr = `${tab.repeat(level)}${prefix}${obj.name}: `;
-    if (obj.children) {
-      return `${startStr}${render(obj.children, level + 1)}`;
-    }
+    if (obj.children) return `${startStr}${render(obj.children, level + 1)}`;
     if (obj.value instanceof Array) {
       const [value1, value2] = [getValue(obj.value[0], level), getValue(obj.value[1], level)];
       return `${tab.repeat(level)}${prefix[0]}${obj.name}: ${value1}\n${tab.repeat(level)}${prefix[1]}${obj.name}: ${value2}`;
     }
-    return `${startStr}${getValue(obj.value, level)}`;
+    return `${startStr}${getValue(obj.value, level, prefix)}`;
   });
 
   return `{\n${res.join('\n')}\n${tab.repeat(level)}}`;
