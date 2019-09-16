298e6a7e403c48fd536682d60f5293848d06f207
upokusaev <upokusaev@gmail.com>
2019-09-16 22:01:24 +0400

    refactoring tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,70 +1,19 @@
+import _ from 'lodash';
 import fs from 'fs';
 import genDiff from '../dist';
 
-const flatResult = `${__dirname}/__fixtures__/flat/result`;
-const flatPlainResult = `${__dirname}/__fixtures__/flat/resultPlain`;
-const flatJsonResult = `${__dirname}/__fixtures__/flat/resultJson`;
-const deepResult = `${__dirname}/__fixtures__/deep/result`;
-const deepPlainResult = `${__dirname}/__fixtures__/deep/resultPlain`;
-const deepJsonResult = `${__dirname}/__fixtures__/deep/resultJson`;
+const path = `${__dirname}/__fixtures__`;
 
-
-test('json', () => {
-  const pathResult = flatResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/flat/before.json`, `${__dirname}/__fixtures__/flat/after.json`)).toBe(strResult);
-});
-
-test('yaml', () => {
-  const pathResult = flatResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/flat/before.yml`, `${__dirname}/__fixtures__/flat/after.yml`)).toBe(strResult);
-});
-
-test('ini', () => {
-  const pathResult = flatResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/flat/before.ini`, `${__dirname}/__fixtures__/flat/after.ini`)).toBe(strResult);
-});
-
-test('deepJson', () => {
-  const pathResult = deepResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/deep/before.json`, `${__dirname}/__fixtures__/deep/after.json`)).toBe(strResult);
-});
-
-test('deepYml', () => {
-  const pathResult = deepResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/deep/before.yml`, `${__dirname}/__fixtures__/deep/after.yml`)).toBe(strResult);
-});
-
-test('deepIni', () => {
-  const pathResult = deepResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/deep/before.ini`, `${__dirname}/__fixtures__/deep/after.ini`)).toBe(strResult);
-});
-
-test('json to Plain', () => {
-  const pathResult = flatPlainResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/flat/before.json`, `${__dirname}/__fixtures__/flat/after.json`, 'plain')).toBe(strResult);
-});
-
-test('deepYml to Plain', () => {
-  const pathResult = deepPlainResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/deep/before.yml`, `${__dirname}/__fixtures__/deep/after.yml`, 'plain')).toBe(strResult);
-});
-
-test('yaml to Json', () => {
-  const pathResult = flatJsonResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/flat/before.yml`, `${__dirname}/__fixtures__/flat/after.yml`, 'json')).toBe(strResult);
-});
-
-test('deepIni to Json', () => {
-  const pathResult = deepJsonResult;
-  const strResult = fs.readFileSync(pathResult, 'ascii');
-  expect(genDiff(`${__dirname}/__fixtures__/deep/before.ini`, `${__dirname}/__fixtures__/deep/after.ini`, 'json')).toBe(strResult);
-});
+test.each([
+  ['json', 'deep', 'custom'], ['json', 'flat', 'plain'],
+  ['yml', 'deep', 'json'], ['yml', 'flat', 'custom'],
+  ['ini', 'deep', 'plain'], ['ini', 'flat', 'json']])(
+  '%s-%s to %s format',
+  (exp, type, format = '') => {
+    const capitalizeFormat = _.capitalize(format);
+    const before = `${path}/${type}/before.${exp}`;
+    const after = `${path}/${type}/after.${exp}`;
+    const expected = fs.readFileSync(`${path}/${type}/result${capitalizeFormat}`, 'ascii');
+    expect(genDiff(before, after, format)).toBe(expected);
+  },
+);
