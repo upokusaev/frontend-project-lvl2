871ef0b5c16c0e0ebda9503bc1b3473df01fdb20
upokusaev <upokusaev@gmail.com>
2019-09-16 22:23:57 +0400

    refactoring of building function

--- src/buildAst.js
+++ src/buildAst.js
@@ -1,6 +1,13 @@
 import _ from 'lodash';
 
-const modEl = [
+const isTwoObj = (obj1, obj2) => (_.isObject(obj1) && _.isObject(obj2));
+
+const modifications = [
+  {
+    mod: 'not',
+    check: (obj1, obj2, key) => isTwoObj(obj1[key], obj2[key]),
+    getValue: () => null,
+  },
   {
     mod: 'not',
     check: (obj1, obj2, key) => (obj1[key] === obj2[key]),
@@ -23,36 +30,7 @@ const modEl = [
   },
 ];
 
-const modObj = [
-  {
-    mod: 'removed',
-    check: (obj1, obj2, key) => (_.has(obj1, key) && !_.has(obj2, key)),
-    getValue: (obj1, obj2, key) => obj1[key],
-  },
-  {
-    mod: 'added',
-    check: (obj1, obj2, key) => (!_.has(obj1, key) && _.has(obj2, key)),
-    getValue: (obj1, obj2, key) => obj2[key],
-  },
-  {
-    mod: 'not',
-    check: (obj1, obj2) => ((_.isObject(obj1) && _.isObject(obj2))),
-    getValue: () => null,
-  },
-  {
-    mod: 'updated',
-    check: (obj1, obj2, key) => (typeof obj1[key] !== typeof obj2[key]),
-    getValue: (obj1, obj2, key) => [obj1[key], obj2[key]],
-  },
-
-];
-
-const isTwoObj = (obj1, obj2) => (_.isObject(obj1) && _.isObject(obj2));
-
-const getMod = (obj1, obj2, key) => {
-  const arrayActions = (isTwoObj(obj1[key], obj2[key])) ? modObj : modEl;
-  return arrayActions.find(({ check }) => check(obj1, obj2, key));
-};
+const getMod = (obj1, obj2, key) => modifications.find(({ check }) => check(obj1, obj2, key));
 
 const buildAst = (before, after) => {
   const keys = Object.keys(before).concat(Object.keys(after));
