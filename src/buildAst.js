c0e61a824a9571496aef92b9d6ae2e1c8e3bb465
upokusaev <upokusaev@gmail.com>
2019-09-25 10:02:15 +0400

    modernization of dynamic dispatching

--- src/buildAst.js
+++ src/buildAst.js
@@ -6,27 +6,32 @@ const objOfTypes = [
   {
     type: 'nested',
     check: (obj1, obj2, key) => isTwoObj(obj1[key], obj2[key]),
-    getValue: () => null,
+    getArrValues: () => [null, null],
+    getChildren: (obj1, obj2, key, buildFunc) => buildFunc(obj1[key], obj2[key]),
   },
   {
     type: 'unchanged',
     check: (obj1, obj2, key) => (obj1[key] === obj2[key]),
-    getValue: (obj1, obj2, key) => obj1[key],
+    getArrValues: (obj1, obj2, key) => [obj1[key], null],
+    getChildren: () => null,
   },
   {
     type: 'removed',
     check: (obj1, obj2, key) => (_.has(obj1, key) && !_.has(obj2, key)),
-    getValue: (obj1, obj2, key) => obj1[key],
+    getArrValues: (obj1, obj2, key) => [null, obj1[key]],
+    getChildren: () => null,
   },
   {
     type: 'added',
     check: (obj1, obj2, key) => (!_.has(obj1, key) && _.has(obj2, key)),
-    getValue: (obj1, obj2, key) => obj2[key],
+    getArrValues: (obj1, obj2, key) => [obj2[key], null],
+    getChildren: () => null,
   },
   {
     type: 'updated',
     check: (obj1, obj2, key) => (obj1[key] !== obj2[key]),
-    getValue: (obj1, obj2, key) => [obj1[key], obj2[key]],
+    getArrValues: (obj1, obj2, key) => [obj2[key], obj1[key]],
+    getChildren: () => null,
   },
 ];
 
@@ -36,12 +41,14 @@ const buildAst = (before, after) => {
   const keys = _.union(Object.keys(before), Object.keys(after)).sort();
   const ast = keys.map((key) => {
     const name = key;
-    const { type, getValue } = getObjType(before, after, key);
-    const children = (type === 'nested') ? buildAst(before[key], after[key]) : null;
-    const value = getValue(before, after, key);
+    const { type, getArrValues, getChildren } = getObjType(before, after, key);
+    const children = getChildren(before, after, key, buildAst);
+    const arrValues = getArrValues(before, after, key);
+    const [value, oldValue] = arrValues;
     return {
       name,
       value,
+      oldValue,
       type,
       children,
     };
