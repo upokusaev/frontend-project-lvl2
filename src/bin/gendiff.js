550a0573bf6d7eba84089de849c9af3eb1f287b5
upokusaev <upokusaev@gmail.com>
2019-09-25 14:20:32 +0400

    format custom replace to default

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,7 +7,7 @@ import genDiff from '..';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-f, --format [type]', 'Output format', 'custom')
+  .option('-f, --format [type]', 'Output format', 'default')
   .arguments('<pathToFile1> <pathToFile2>')
   .action((pathToFile1, pathToFile2) => {
     console.log(genDiff(pathToFile1, pathToFile2, program.format));
