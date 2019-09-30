57d8a77fe84e127ea131688200486d39f53becc0
upokusaev <upokusaev@gmail.com>
2019-09-30 09:15:20 +0400

    rename custom to tree

---
+++ src/formatters/renderTree.js
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+
+const getPrefix = (obj) => {
+  switch (obj.type) {
+    case 'removed': return '  - ';
+    case 'added': return '  + ';
+    case 'updated': return ['  + ', '  - '];
+    default: return '    ';
+  }
+};
+
+const tab = '    ';
+
+const expandValue = (obj, level) => {
+  const res = Object.keys(obj).map((key) => `${tab.repeat(level + 1)}${key}: ${obj[key]}`);
+  return `{\n${res.join('\n')}\n${tab.repeat(level)}}`;
+};
+
+const getValue = (value, level) => {
+  if (value instanceof Object) return expandValue(value, level + 1);
+  return value;
+};
+
+const render = (diff, level = 0) => {
+  const res = diff.map((obj) => {
+    const prefix = getPrefix(obj);
+    const startStr = `${tab.repeat(level)}${prefix}${obj.name}: `;
+    if (obj.type === 'nested') return `${startStr}${render(obj.children, level + 1)}`;
+    if (obj.type === 'updated') {
+      const arrValues = [obj.value, obj.oldValue];
+      const arrStr = arrValues.map((value, i) => `${tab.repeat(level)}${prefix[i]}${obj.name}: ${getValue(value, level)}`);
+      return _.reverse(arrStr).join('\n');
+    }
+    if (obj.type === 'removed') return `${startStr}${getValue(obj.oldValue, level, prefix)}`;
+    if (obj.type === 'added') return `${startStr}${getValue(obj.value, level, prefix)}`;
+    return `${startStr}${getValue(obj.value, level, prefix)}`;
+  });
+
+  return `{\n${res.join('\n')}\n${tab.repeat(level)}}`;
+};
+
+const normalizeRezult = (diff) => `${render(diff)}\n`;
+
+export default normalizeRezult;
