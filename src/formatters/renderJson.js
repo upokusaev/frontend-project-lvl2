ec8e5cf4643121ca1c9b5b8cc71e7943443b7c4d
upokusaev <upokusaev@gmail.com>
2019-10-08 09:40:22 +0400

    json stringify

--- src/formatters/renderJson.js
+++ src/formatters/renderJson.js
@@ -1,28 +1,4 @@
-const expandValue = (value) => {
-  if (value instanceof Object) {
-    const obj = value;
-    const res = Object.keys(obj).map((key) => `"${key}": "${obj[key]}"`);
-    return `{ ${res.join(', ')} }`;
-  }
-  return (value === null) ? '""' : `"${value}"`;
-};
-
-const getValue = (obj) => {
-  if (obj.type === 'updated') return `"newValue": ${expandValue(obj.newValue)}, "oldValue": ${expandValue(obj.oldValue)} `;
-  const value = (obj.type === 'removed') ? obj.oldValue : obj.newValue;
-  return `"value": ${expandValue(value)}`;
-};
-
-const render = (diff) => {
-  const res = Object.keys(diff).map((key) => {
-    const obj = diff[key];
-    const name = `"name": "${obj.name}"`;
-    const type = `"type": "${obj.type}"`;
-    const content = (obj.type === 'nested') ? `"children": ${render(obj.children)}` : getValue(obj);
-    return `{ ${name}, ${type}, ${content} }`;
-  });
-  return `[${res.join(',')}]`;
-};
+const render = (diff) => JSON.stringify(diff);
 
 const normalizeResult = (diff) => `${render(diff)}\n`;
 
