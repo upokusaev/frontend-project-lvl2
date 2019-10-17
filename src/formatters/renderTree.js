de9e19639f83516f756d2aecf100d8a23ac47115
upokusaev <upokusaev@gmail.com>
2019-10-17 09:07:39 +0400

    refactory

--- src/formatters/renderTree.js
+++ src/formatters/renderTree.js
@@ -1,43 +1,32 @@
 import _ from 'lodash';
 
 const tab = '    ';
-const getPrefix = (obj) => {
-  switch (obj.type) {
-    case 'removed': return '  - ';
-    case 'added': return '  + ';
-    case 'updated': return ['  - ', '  + '];
-    default: return '    ';
-  }
-};
 
-const expandValue = (obj, depth) => {
+const stringify = (obj, depth) => {
   const res = Object.keys(obj).map((key) => `${tab.repeat(depth + 1)}${key}: ${obj[key]}`);
   return `{\n${res.join('\n')}\n${tab.repeat(depth)}}`;
 };
 
-const getValue = (value, depth) => {
-  if (value instanceof Object) return expandValue(value, depth + 1);
-  return value;
-};
+const getValue = (value, depth) => (
+  (value instanceof Object) ? stringify(value, depth + 1) : value
+);
 
 const render = (diff, depth = 0) => {
   const deepResult = diff.map((obj) => {
-    const prefix = getPrefix(obj);
-    const name = `${tab.repeat(depth)}${prefix}${obj.name}: `;
     switch (obj.type) {
       case 'nested':
-        return `${name}${render(obj.children, depth + 1)}`;
+        return `${tab.repeat(depth)}    ${obj.name}: ${render(obj.children, depth + 1)}`;
       case 'updated': {
         return [
-          `${tab.repeat(depth)}${prefix[0]}${obj.name}: ${getValue(obj.oldValue, depth)}`,
-          `${tab.repeat(depth)}${prefix[1]}${obj.name}: ${getValue(obj.newValue, depth)}`];
+          `${tab.repeat(depth)}  - ${obj.name}: ${getValue(obj.oldValue, depth)}`,
+          `${tab.repeat(depth)}  + ${obj.name}: ${getValue(obj.newValue, depth)}`];
       }
       case 'removed':
-        return `${name}${getValue(obj.oldValue, depth)}`;
+        return `${tab.repeat(depth)}  - ${obj.name}: ${getValue(obj.oldValue, depth)}`;
       case 'added':
-        return `${name}${getValue(obj.newValue, depth)}`;
+        return `${tab.repeat(depth)}  + ${obj.name}: ${getValue(obj.newValue, depth)}`;
       case 'unchanged':
-        return `${name}${getValue(obj.newValue, depth)}`;
+        return `${tab.repeat(depth)}    ${obj.name}: ${getValue(obj.newValue, depth)}`;
       default:
         throw new Error(`Error: unexpected type "${obj.type}"`);
     }
